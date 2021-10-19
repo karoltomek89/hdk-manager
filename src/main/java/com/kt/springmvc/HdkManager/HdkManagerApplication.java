a24e7da330a1319ec47ef3c4e989fe3b174d06c0
karoltomek <karoltomek@o2.pl>
2021-10-19 12:42:04 +0200

    default classes from start.spring.io

---
+++ src/main/java/com/kt/springmvc/HdkManager/HdkManagerApplication.java
@@ -0,0 +1,13 @@
+package com.kt.springmvc.HdkManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdkManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdkManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kt/springmvc/HdkManager/HdkManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt.springmvc.HdkManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdkManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
